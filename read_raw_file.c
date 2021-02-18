9923e17ec45b6d0350bf27d17d469ae6b80e864b
Cherry Ng <cherrywyng@gmail.com>
2021-02-18 20:08:27 +0200

    Read raw files

--- read_raw_file.c
+++ read_raw_file.c
@@ -10,7 +10,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include "hashpipe.h"
-#include "readfile_databuf.h"
+#include "hpguppi_databuf.h"
 #include "rawspec_rawutils.h"
 
 ssize_t read_fully(int fd, void * buf, size_t bytes_to_read)
@@ -37,7 +37,7 @@ ssize_t read_fully(int fd, void * buf, size_t bytes_to_read)
 
 static void *run(hashpipe_thread_args_t * args)
 {
-    readfile_input_databuf_t *db  = (readfile_input_databuf_t *)args->obuf;
+    hpguppi_input_databuf_t *db  = (hpguppi_input_databuf_t *)args->obuf;
     hashpipe_status_t st = args->st;
     const char * status_key = args->thread_desc->skey;
 
@@ -49,6 +49,9 @@ static void *run(hashpipe_thread_args_t * args)
     int fdin;
     rawspec_raw_hdr_t raw_hdr;
     size_t bytes_read;
+    char *ptr;
+    int len;
+    int directio = 0;
     while (run_threads()) {
 
         hashpipe_status_lock_safe(&st);
@@ -61,7 +64,7 @@ static void *run(hashpipe_thread_args_t * args)
         /* Wait for new block to be free, then clear it
          * if necessary and fill its header with new values.
          */
-        while ((rv=readfile_input_databuf_wait_free(db, block_idx)) 
+        while ((rv=hpguppi_input_databuf_wait_free(db, block_idx)) 
                 != HASHPIPE_OK) {
             if (rv==HASHPIPE_TIMEOUT) {
                 hashpipe_status_lock_safe(&st);
@@ -76,7 +79,8 @@ static void *run(hashpipe_thread_args_t * args)
         }
 
 	//Read raw files
-	fdin = open("/datax/20200626/commensal/Unknown/GUPPI/guppi_59025_41038_003850_Unknown_0001.0000.raw", O_RDONLY);
+	//fdin = open("/datax/20200626/commensal/Unknown/GUPPI/guppi_59025_41038_003850_Unknown_0001.0000.raw", O_RDONLY);
+	fdin = open("/buf0/scratch/Cherry/Data/guppi_59204_70813_002321__0001.0000.raw", O_RDONLY);
 	if(fdin == -1) {
 	  hashpipe_error(__FUNCTION__, "cannot open raw file");
 	  pthread_exit(NULL);
@@ -104,29 +108,31 @@ static void *run(hashpipe_thread_args_t * args)
         hashpipe_status_unlock_safe(&st);
 	
 
-	//Read data (say 200B for now)
-	int input_len = 200;
-	unsigned char input_data[input_len];
-	bytes_read = read_fully(fdin,input_data, input_len);
+	//Read data
+	//int nfreq = raw_hdr.obsnchan/raw_hdr.nants;
+	ptr = hpguppi_databuf_data(db, block_idx);
+	len = raw_hdr.blocsize;
+	printf("len is %d; directio is %d\n", len, directio);
+	if(directio) {
+	  // Round up to next multiple of 512
+	  len = (len+511) & ~511;
+	}
+	bytes_read = read_fully(fdin, ptr, len);
 	close(fdin);
-
-	//move these two numbers to buffer
-	db->block[block_idx].header.mcnt = mcnt;
-	memcpy(db->block[block_idx].data_block, input_data, input_len*sizeof(unsigned char));
 
 	//display some values
 	hashpipe_status_lock_safe(&st);
-	hputi8(st.buf,"data[0]",input_data[0]);
-	hputi8(st.buf,"data[1]",input_data[1]);
-	hputi8(st.buf,"data[-2]",input_data[input_len-2]);
-	hputi8(st.buf,"data[-1]",input_data[input_len-1]);
+	//hputi8(st.buf,"data[0]",db[0]);
+	//hputi8(st.buf,"data[1]",db[1]);
+	//hputi8(st.buf,"data[-2]",input_data[input_len-2]);
+	//hputi8(st.buf,"data[-1]",input_data[input_len-1]);
 
 	hashpipe_status_unlock_safe(&st);
         // Mark block as full
-        readfile_input_databuf_set_filled(db, block_idx);
+        hpguppi_input_databuf_set_filled(db, block_idx);
 
         // Setup for next block
-        block_idx = (block_idx + 1) % db->header.n_block;
+        block_idx = (block_idx + 1) % N_INPUT_BLOCKS;
         mcnt++;
 
         /* Will exit if thread has been cancelled */
@@ -143,7 +149,7 @@ static hashpipe_thread_desc_t read_raw_file = {
     init: NULL,
     run:  run,
     ibuf_desc: {NULL},
-    obuf_desc: {readfile_input_databuf_create}
+    obuf_desc: {hpguppi_input_databuf_create}
 };
 
 static __attribute__((constructor)) void ctor()
---
+++ readfile_init.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/cherryng/softwares/hashpipe/bin/hashpipe -p readfile_hashpipe -I 0 read_raw_file
